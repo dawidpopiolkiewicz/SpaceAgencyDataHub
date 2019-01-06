69938fd59146b7d75b20a2d005fab5c4f320bb64
Dawid <dawid.popiolkiewicz@gmail.com>
2019-01-06 15:29:21 +0100

    update searchProduct method

--- src/main/java/com/spaceagencydatahub/rest/ProductController.java
+++ src/main/java/com/spaceagencydatahub/rest/ProductController.java
@@ -1,10 +1,11 @@
 package com.spaceagencydatahub.rest;
 
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.spaceagencydatahub.entity.Product;
 import com.spaceagencydatahub.service.ProductService;
+import com.spaceagencydatahub.util.OffsetDateTimeConverterUtil;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +25,11 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
+
+	@Autowired
+	private OffsetDateTimeConverterUtil offSetDateTimeConverterUtil;
+
+	private static Logger logger = Logger.getLogger(ProductController.class.getName());
 
 	@GetMapping("/products")
 	public List<Product> getProduct(@RequestParam(value = "id") List<Integer> ids) {
@@ -32,8 +39,10 @@ public class ProductController {
 	@GetMapping("/products/search")
 	public List<Product> searchProduct(@RequestParam(value = "missionName") String missionName,
 			@RequestParam(value = "productType") String productType,
-			@RequestParam(value = "acquisitionDate") OffsetDateTime acquisitionDate
-	) {
+			@RequestParam(value = "acquisitionDate") String date) {
+
+		OffsetDateTime acquisitionDate = offSetDateTimeConverterUtil.convertStringToOffsetDateTime(date);
+		logger.info("OffsetDateTime: "+ acquisitionDate);
 		return productService.searchProduct(missionName, productType, acquisitionDate);
 	}
 
